09a3f71352409e5e5a7ed2bcb87e5ca1e6172270
Sergei O. Udalov <sergei.udalov@gmail.com>
2010-03-31 03:50:32 +0400

    fantom_block_for takes string/block

--- lib/fantom.rb
+++ lib/fantom.rb
@@ -1,8 +1,10 @@
 module FantomControls
   module Helpers
-    def fantom_block_for(groups, &block)
+    def fantom_block_for(groups, content=nil, &block)
       if block_given?
-        content_tag(:div, :class => "fantom " + groups.to_a.map{|g| "fantom_access_group_" + g.to_s}.join(" "), &block)
+        content_tag(:span, :class => "fantom " + groups.to_a.map{|g| "fantom_access_group_" + g.to_s}.join(" "), &block)
+      elsif !content.nil?
+        content_tag(:span, content, :class => "fantom " + groups.to_a.map{|g| "fantom_access_group_" + g.to_s}.join(" "))
       else
         nil
       end
