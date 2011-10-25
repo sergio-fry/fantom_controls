6dcf5f575514bb56022b978ff31a3204f5e9eb7d
Sergei O. Udalov <sergei.udalov@gmail.com>
2011-10-25 13:17:30 +0400

    noindex added

--- lib/fantom.rb
+++ lib/fantom.rb
@@ -2,9 +2,13 @@ module FantomControls
   module Helpers
     def fantom_block_for(groups, content=nil, &block)
       if block_given?
-        content_tag(:span, :class => "fantom " + groups.to_a.map{|g| "fantom_access_group_" + g.to_s}.join(" "), &block)
+        content_tag(:noindex) do
+          content_tag(:span, :class => "fantom " + groups.to_a.map{|g| "fantom_access_group_" + g.to_s}.join(" "), &block)
+        end
       elsif !content.nil?
-        content_tag(:span, content, :class => "fantom " + groups.to_a.map{|g| "fantom_access_group_" + g.to_s}.join(" "))
+        content_tag(:noindex) do
+          content_tag(:span, content, :class => "fantom " + groups.to_a.map{|g| "fantom_access_group_" + g.to_s}.join(" "))
+        end
       else
         nil
       end
